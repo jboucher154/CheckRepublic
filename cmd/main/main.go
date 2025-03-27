0f9cbc70ef926e5d016d0461508f4ff08f056b4c
jen <jboucher154@gmail.com>
2025-03-27 12:10:09 +0200

    add package declarations to go files

---
+++ cmd/main/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	
+	// adds a new handler
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "This should serve the root")
+	})
+
+	// serve static files by setting up a file server directory
+	fs := http.FileServer(http.Dir("static/"))
+	// point url to the file path with and handle function
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// start up server
+	http.ListenAndServe(":80", nil)
+}
