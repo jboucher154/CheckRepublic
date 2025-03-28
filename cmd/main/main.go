df18db76af24a7bc2383c7de81b8c48354b09d7d
jen <jboucher154@gmail.com>
2025-03-28 19:53:08 +0200

    change handler assignment to serve specifed uri

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -13,8 +13,9 @@ func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "This should serve the root")
 	})
-	handler := http.HandlerFunc(api.ChecklistHandler)
-
+	// can pass idectly to the listen and serve for root behavior, but can also add as a specific endpoint
+	// handler := http.HandlerFunc(api.ChecklistHandler)
+	http.HandleFunc("/checklist", api.ChecklistHandler)
 	//****look into this more for static assests
 	// serve static files by setting up a file server directory
 	// fs := http.FileServer(http.Dir("static/"))
@@ -22,5 +23,5 @@ func main() {
 	// http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// start up server
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
