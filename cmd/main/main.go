260315e4bd6956df1dd94b9bc963811b8ac9e0b6
jen <jboucher154@gmail.com>
2025-03-28 19:45:26 +0200

    remove unneeded module files

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"check_republic/internal/api"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -11,12 +13,14 @@ func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "This should serve the root")
 	})
+	handler := http.HandlerFunc(api.ChecklistHandler)
 
+	//****look into this more for static assests
 	// serve static files by setting up a file server directory
-	fs := http.FileServer(http.Dir("static/"))
+	// fs := http.FileServer(http.Dir("static/"))
 	// point url to the file path with and handle function
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// start up server
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
